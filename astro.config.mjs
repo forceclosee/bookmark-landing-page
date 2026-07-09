77288e3ae3d518e57d18e4806506fa355145dbdd
ForceClose <273080622+forceclosee@users.noreply.github.com>
2026-07-09 11:11:34 +0700

    style(formatting): format codebase files to match 'Biome' rules
    - Convert indentation from spaces to tabs in 'tsconfig.json' and multiple Astro and CSS stylesheets
    - Reformat 'src/pages/index.astro' and 'src/layouts/Layout.css' to follow tab-based style rules

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,13 @@ import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  fonts: [
-    {
-      provider: fontProviders.fontsource(),
-      name: "Rubik",
-      cssVariable: "--font-rubik",
-      weights: ["300 900"],
-      fallbacks: ["sans-serif"],
-    },
-  ],
+	fonts: [
+		{
+			provider: fontProviders.fontsource(),
+			name: "Rubik",
+			cssVariable: "--font-rubik",
+			weights: ["300 900"],
+			fallbacks: ["sans-serif"],
+		},
+	],
 });
