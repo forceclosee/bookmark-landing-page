64d1df64b308eaac23610b3b27201c4c94a9f87e
ForceClose <273080622+forceclosee@users.noreply.github.com>
2026-07-13 09:48:18 +0700

    feat: add 'Extension' download section and integrate into home page
    - Create 'Extension.astro' component and 'Extension.css' stylesheet
    - Create browser data in 'data-extension.ts' to populate extension cards
    - Render 'Extension' component in 'index.astro'

---
+++ src/data/data-extension.ts
@@ -0,0 +1,27 @@
+import chromeLogo from "@img/logo-chrome.svg";
+import firefoxLogo from "@img/logo-firefox.svg";
+import operaLogo from "@img/logo-opera.svg";
+
+export const extension = [
+	{
+		image: chromeLogo,
+		title: "Add to Chrome",
+		minVersion: "Minimum version 62",
+		width: 102,
+		height: 100,
+	},
+	{
+		image: firefoxLogo,
+		title: "Add to Firefox",
+		minVersion: "Minimum version 55",
+		width: 105,
+		height: 100,
+	},
+	{
+		image: operaLogo,
+		title: "Add to Opera",
+		minVersion: "Minimum version 46",
+		width: 96,
+		height: 100,
+	},
+];
