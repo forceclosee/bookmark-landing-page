ae4e19cc783b285be3eff9a3b94a84df1c97f558
ForceClose <273080622+forceclosee@users.noreply.github.com>
2026-08-18 09:43:33 +0700

    chore(types): support 'command' and 'commandfor' on 'ButtonHTMLAttributes'
    - Import 'react' and extend 'ButtonHTMLAttributes' with 'command' and 'commandfor' attributes
    - Move 'RequireAttributes' utility type into 'declare global' block in 'src/env.d.ts'

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,15 +1,29 @@
-/**
- * utility to make attribute required
- *
- * @example
- * to make "href" and "target" required
- * ```tsx
- * interface Props extends MakeRequired<HTMLAttributes<"a">, "href" | "target"> {custom props here};
- * ```
- * or
- * ```tsx
- * type Props = RequireAttributes<HTMLAttributes<"a">, "href" | "target"> & {custom props here};
- * ```
- */
-type RequireAttributes<T, K extends keyof T> = Omit<T, K> &
-	Required<Pick<T, K>>;
+import "react";
+
+declare module "react" {
+	/**
+	 * declare command and commandfor attribute to button element
+	 */
+	interface ButtonHTMLAttributes<T> extends HTMLAttributes<T> {
+		command?: string;
+		commandfor?: string;
+	}
+}
+
+declare global {
+	/**
+	 * utility to make attribute required
+	 *
+	 * @example
+	 * to make "href" and "target" required
+	 * ```tsx
+	 * interface Props extends MakeRequired<HTMLAttributes<"a">, "href" | "target"> {custom props here};
+	 * ```
+	 * or
+	 * ```tsx
+	 * type Props = RequireAttributes<HTMLAttributes<"a">, "href" | "target"> & {custom props here};
+	 * ```
+	 */
+	type RequireAttributes<T, K extends keyof T> = Omit<T, K> &
+		Required<Pick<T, K>>;
+}
