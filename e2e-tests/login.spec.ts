5e3c1973a4e3b3b3ba6a91575c0a066fe6f9b0dd
ForceClose <273080622+forceclosee@users.noreply.github.com>
2026-08-28 09:18:46 +0700

    test(e2e): use env credentials and a11y locators

--- e2e-tests/login.spec.ts
+++ e2e-tests/login.spec.ts
@@ -1,11 +1,19 @@
 import { test, expect, type Page } from "@playwright/test";
 
+const userName = process.env.TEST_USER_NAME;
+const userEmail = process.env.TEST_USER_EMAIL;
+const userPassword = process.env.TEST_USER_PASSWORD;
+
+if (!userName || !userEmail || !userPassword) {
+	throw new Error("Name, Email, and Password is missing");
+}
+
 test.describe("Login Flow", () => {
 	// valid data object for base template
 	const getValidData = () => ({
-		loginName: "Example",
-		loginEmail: "e@e.example",
-		loginPassword: "aaaaa1W!",
+		loginName: userName,
+		loginEmail: userEmail,
+		loginPassword: userPassword,
 	});
 
 	// Helper function to fill form
@@ -13,30 +21,38 @@ test.describe("Login Flow", () => {
 		page: Page,
 		data: ReturnType<typeof getValidData>,
 	) {
-		await page.getByTestId("login-email").fill(data.loginEmail);
-		await page.getByTestId("login-password").fill(data.loginPassword);
+		await page.getByRole("dialog").getByLabel("Email").fill(data.loginEmail);
+		await page
+			.getByRole("dialog")
+			.getByPlaceholder("Enter your password")
+			.fill(data.loginPassword);
 	}
 
-	test.beforeEach(async ({ page, context }) => {
+	test.beforeEach(async ({ page, context, isMobile }) => {
 		// Clear session cookie
 		await context.clearCookies();
 
 		// Go to home page
 		await page.goto("/");
 
-		// Open login modal
+		// login button
 		const openLoginModalButton = page
-			.getByTestId("open-login-modal-button")
-			.first();
-		await openLoginModalButton.waitFor({ state: "visible" });
-		await openLoginModalButton.click();
+			.getByRole("button", { name: "Login" })
+			.filter({ visible: true });
 
-		// Wait for modal to be visible
-		const modal = page.getByTestId("login-modal");
-		await modal.waitFor({ state: "visible" });
+		// Open login modal mobile
+		if (isMobile) {
+			await page.getByRole("button", { name: "Open menu" }).click();
+			await openLoginModalButton.click();
+		}
+
+		// Open login modal desktop
+		if (!isMobile) {
+			await openLoginModalButton.click();
+		}
 
 		// Verify modal is open
-		await expect(modal).toBeVisible;
+		await expect(page.getByRole("dialog")).toBeVisible;
 	});
 
 	test("should successfully login with valid credentials", async ({ page }) => {
@@ -47,38 +63,35 @@ test.describe("Login Flow", () => {
 
 		// Click submit button
 		await page
-			.getByTestId("login-modal")
+			.getByRole("dialog")
 			.getByRole("button", { name: "Login" })
 			.click();
 
 		// Verify toast message
-		const successToast = page
-			.getByTestId("login-modal")
-			.getByTestId("toast-message-text");
-		await expect(successToast).toHaveText(`Welcome back ${data.loginName}`);
+		await expect(page.getByRole("dialog").getByRole("status")).toContainText(
+			`Welcome back ${data.loginName}`,
+		);
 	});
 
 	test("should fail validation when email address is invalid", async ({
 		page,
 	}) => {
 		// only test 1 scenario, all other scenario allready pass in unit test
-		const data = { ...getValidData(), loginEmail: "@example.com" };
+		const data = { ...getValidData(), loginEmail: "invalid@email" };
 
 		// Fill form
 		await fillLoginForm(page, data);
 
 		// Click submit button
 		await page
-			.getByTestId("login-modal")
+			.getByRole("dialog")
 			.getByRole("button", { name: "Login" })
 			.click();
 
 		// Verify error message
-		const ErrorEmail = page
-			.getByTestId("form-field")
-			.filter({ has: page.getByTestId("login-email") })
-			.getByTestId("input-error-message");
-		await expect(ErrorEmail).toHaveText("Please enter a valid email address");
+		await expect(
+			page.getByRole("dialog").getByLabel("Email"),
+		).toHaveAccessibleDescription("Please enter a valid email address");
 	});
 
 	test("should fail validation when password is empty", async ({ page }) => {
@@ -89,15 +102,13 @@ test.describe("Login Flow", () => {
 
 		// Click submit button
 		await page
-			.getByTestId("login-modal")
+			.getByRole("dialog")
 			.getByRole("button", { name: "Login" })
 			.click();
 
 		// Verify error message
-		const ErrorPassword = page
-			.getByTestId("form-field")
-			.filter({ has: page.getByTestId("login-password") })
-			.getByTestId("input-error-message");
-		await expect(ErrorPassword).toHaveText("Password cannot empty");
+		await expect(
+			page.getByRole("dialog").getByPlaceholder("Enter your password"),
+		).toHaveAccessibleDescription("Password cannot empty");
 	});
 });
